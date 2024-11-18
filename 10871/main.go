79ee3796132ff90f50a1f8c59d9c878f15de3516
SeungYup Ryu <n006h3417h@gmail.com>
2024-11-18 14:39:45 +0900

    Solve 10871

---
+++ 10871/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n, x int
+	fmt.Scan(&n, &x)
+
+	a := make([]int, n)
+	for i := range a {
+		fmt.Scan(&a[i])
+	}
+
+	for _, v := range a {
+		if v < x {
+			fmt.Print(v, " ")
+		}
+	}
+}
